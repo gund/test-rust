ce2dcdcdb2055d91f60dc3378f0e29bd44deb2a4
Alex Malkevich <malkevich.alex@gmail.com>
2022-04-27 21:50:26 +0200

    perf: use non-locked stdout

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
+extern crate kernel32;
+extern crate winapi;
+
 use std::fs::File;
 use std::io::{self, Write};
-use std::os::unix::io::FromRawFd;
 
 fn main() -> io::Result<()> {
-    let mut stdout = unsafe { File::from_raw_fd(1) };
+    let mut stdout = getRawStdout();
+    write!(stdout, "Hello, world!")?;
     stdout.write_all(b"Hello, world!")?;
     Ok(())
+}
 
-    // println!("Hello, world!");
-    // io::stdout().write(b"Hello, world!")?;
+#[cfg(unix)]
+fn getRawStdout() -> File {
+    use std::os::unix::io::FromRawFd;
+    unsafe { File::from_raw_fd(1) }
+}
+
+#[cfg(windows)]
+fn getRawStdout() -> File {
+    use std::os::windows::io::FromRawHandle;
+    let h = kernel32::GetStdHandle(winapi::winbase::STD_OUTPUT_HANDLE);
+    File::from_raw_handle(h);
 }
