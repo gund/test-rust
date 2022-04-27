85c85819319c71a958c9ce4d8a929caf76e82f39
Alex Malkevich <malkevich.alex@gmail.com>
2022-04-27 21:24:53 +0200

    perf: use non-locked write to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, Write};
+use std::os::unix::io::FromRawFd;
+
+fn main() -> io::Result<()> {
+    let mut stdout = unsafe { File::from_raw_fd(1) };
+    stdout.write_all(b"Hello, world!")?;
+    Ok(())
+
+    // println!("Hello, world!");
+    // io::stdout().write(b"Hello, world!")?;
 }
